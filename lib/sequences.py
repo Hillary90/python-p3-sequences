344ebf167c32a1779cb9348c117a749005f3de96
Hillary Tanui <hillarytanui80@gmail.com>
2025-11-12 11:22:43 +0300

    completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
-#!/usr/bin/env python3
+def fibonacci(length):
+    fib_sequence = [0, 1]
+    while len(fib_sequence) < length:
+        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+    return fib_sequence
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    else:
+        fib_sequence = fibonacci(length)
+        print(fib_sequence)
